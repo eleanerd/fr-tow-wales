43485dbcd1becb02bbb29b37e16220eae1cc0ffa
eleanordowner <eleanor.downer@ForestResearch.gov.uk>
2025-10-22 14:20:55 +0100

    Run TOW_Wales_pPt1.py for all tiles with Planet SR basemap coverage

---
+++ pipeline/tow_wales_pipeline.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# List files in dir
+dir="C:/Users/eleanor.downer/OneDrive - Forest Research/Documents/TOW_Wales/data/"
+files=$(ls "$dir")
+
+# Extract tile names
+tiles=()
+for file in $files; do
+    if [[ $file == *_CHM.tif ]]; then
+        tile_name=${file%%_*}
+        tiles+=("$tile_name")
+        
+        python Wales_VOM_Pt1.py "$tile_name"
+    fi
+done 
