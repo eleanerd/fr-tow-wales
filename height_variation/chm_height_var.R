9ab3fca4e488e9029175fb6599c339837f215da6
eleanordowner <eleanor.downer@ForestResearch.gov.uk>
2025-11-06 10:44:04 +0000

    Condition added to check if tiles already processed

--- height_variation/chm_height_var.R
+++ height_variation/chm_height_var.R
@@ -7,22 +7,27 @@ library(glue)
 library(plyr)
 library(sf)
 
-tile_of_interest <- commandArgs(trailingOnly = TRUE)
-wd <- "Y:/Forest Inventory/0700_NonCore_Funded/0726_TOW_Wales/04_Spatial Analysis/3_Test_Square"
+wd <- "Y:/Forest Inventory/0700_NonCore_Funded/0726_TOW_Wales/04_Spatial Analysis/1_Reference_Data/0_VOM/with_nfi"
 
 # read in files
-chm_dir <- glue("{wd}/VOM/{tile_of_interest}_VOM_with_NFI.tif")
-chm_fns <- list.files(chm_dir, pattern = "*_VOM_with_NFI_NDVI_masked.tif$", full.names=TRUE)
+chm_fns <- list.files(wd, pattern = "*with_NFI.tif$", full.names=TRUE)
 
 window_size <- 3
 window <- matrix(1, nrow = window_size, ncol = window_size)
 
 for (chm_fn in chm_fns) {
-
+  
+  tile <- strsplit(basename(chm_fn), "_")[[1]][[1]]
+  filepath <- glue("{wd}/height_var/{tile}_height_var.tif")
+  
+  if (file.exists(filepath) {
+    next
+  }
+  
   chm <- terra::rast(chm_fn)
   r_var <- focal(chm, w = window, fun = sd, na.policy = "omit", na.rm = TRUE)
 
-  filepath <- glue("{chm_dir}/height_var/SS79_hv3x3.tif")
   writeRaster(r_var, filepath, overwrite=TRUE)
 
 }
+ 
